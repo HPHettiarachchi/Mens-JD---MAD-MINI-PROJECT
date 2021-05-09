689890646f97808b9eb279358222b9b58854d0b8
shakindiAbeykoon <76742821+shakindiAbeykoon@users.noreply.github.com>
2021-05-09 23:13:22 +0530

    Login and Registration

---
+++ app/src/main/java/com/example/pantsqlitedemo/LoginSucess.java
@@ -0,0 +1,14 @@
+package com.wondrous.jdfashionhome;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginSucess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sucess);
+    }
+}
